5ca4a0be8c0afc6b4b373721fc403cccb113f840
Berat Çimen <47108366+beratcmn@users.noreply.github.com>
2021-08-23 16:06:39 +0300

    Added the wake system
    To change the wake keyword change the "wakeSentence" variable.

--- VoiceEngine.py
+++ VoiceEngine.py
@@ -1,27 +1,28 @@
 # ------------------------- Imports -------------------------
 # Voice Engine libraries
+from types import TracebackType
 from gtts import gTTS
 import speech_recognition as sr
 import vlc
 # General libraries
 import time
-from datetime import date
 import os
 import sys
 
 # ------------------------- Global Variables -------------------------
 r = sr.Recognizer()
 
-today = date.today()
-today = str(today).replace("-", " ")
-
-nameCounter = 0
-
 vlc_instance = vlc.Instance()
 player = vlc_instance.media_player_new()
 
+nameCounter = 0
+
+wakeSentence = "Arif"
+
+inConversation = False
 
 # ------------------------- Functions -------------------------
+
 
 def Talk(sentence):
     global nameCounter
@@ -29,30 +30,65 @@ def Talk(sentence):
     global player
     nameCounter = nameCounter + 1
     fileName = "ses" + str(nameCounter) + ".wav"
-    tts = gTTS(sentence, lang="tr", slow=False)
-    tts.save(fileName)
-    time.sleep(0.1)
-    media = vlc_instance.media_new(fileName)
-    player.set_media(media)
-    player.play()
-    time.sleep(0.5)
-    duration = player.get_length()
-    time.sleep(duration / 1000 - 0.5)
-    player.stop()
-    os.remove(fileName)
+    try:
+        tts = gTTS(sentence, lang="tr", slow=False)
+        tts.save(fileName)
+        time.sleep(0.1)
+        media = vlc_instance.media_new(fileName)
+        player.set_media(media)
+        player.play()
+        time.sleep(0.5)
+        duration = player.get_length()
+        time.sleep(duration / 1000 - 0.5)
+        player.stop()
+        os.remove(fileName)
+    except Exception as e:
+        print(e)
 
 
 def Listen():
-    with sr.Microphone() as source:
-        print("Konuşabilirsiniz...")
-        r.adjust_for_ambient_noise(source)
-        try:
-            voice = r.listen(source, timeout=2, phrase_time_limit=5)
-            voice_data = r.recognize_google(voice, language="tr-tr")
-            return voice_data
-        except sr.WaitTimeoutError:
-            Talk("Sizi dinlerken zaman aşımına uğradım.")
-        except sr.UnknownValueError:
-            Talk("Ne dediğinizi anlayamadım.")
-        except sr.RequestError:
-            print("İnternet bağlanırken bir hata oluştu. Lütfen tekrar deneyiniz.")
+    global inConversation
+    global wakeSentence
+    global vlc_instance
+    global player
+
+    if inConversation == False:
+        with sr.Microphone() as source:
+            print("Bekleme modundayım ve sizi dinliyorum...")
+            r.adjust_for_ambient_noise(source)
+            try:
+                voice = r.listen(source, timeout=7, phrase_time_limit=4)
+                voice_data = r.recognize_google(voice, language="tr-tr")
+                print(voice_data)
+                if wakeSentence in voice_data:
+                    inConversation = True
+                    Talk("Efendim")
+            except sr.WaitTimeoutError:
+                pass
+            except sr.UnknownValueError:
+                pass
+            except sr.RequestError as e:
+                print(e)
+    else:
+        media = vlc_instance.media_new("beep.mp3")
+        player.set_media(media)
+        player.play()
+        time.sleep(0.5)
+        duration = player.get_length()
+        time.sleep(duration/1000 - 0.5)
+        player.stop()
+        time.sleep(0.05)
+
+        with sr.Microphone() as source:
+            print("Konuşabilirsiniz...")
+            r.adjust_for_ambient_noise(source)
+            try:
+                voice = r.listen(source, timeout=10, phrase_time_limit=10)
+                voice_data = r.recognize_google(voice, language="tr-tr")
+                print("Siz: " + str(voice_data))
+                return voice_data
+            except:
+                inConversation = False
+
+
+# ------------------------- Program -------------------------
