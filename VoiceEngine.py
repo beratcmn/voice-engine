d1e4911d71c23ac08049fb2a0c79a8699a6523c9
Berat Çimen <47108366+beratcmn@users.noreply.github.com>
2021-09-02 14:57:54 +0300

    Add files via upload

--- VoiceEngine.py
+++ VoiceEngine.py
@@ -26,6 +26,7 @@ def Talk(sentence):
     global nameCounter
     global vlc_instance
     global player
+    sentence = str(sentence)
     nameCounter = nameCounter + 1
     fileName = "ses" + str(nameCounter) + ".wav"
     try:
@@ -37,7 +38,7 @@ def Talk(sentence):
         player.play()
         time.sleep(0.5)
         duration = player.get_length()
-        time.sleep(duration / 1000 - 0.5)
+        time.sleep(duration / 1000 - 0.4)
         player.stop()
         os.remove(fileName)
     except Exception as e:
@@ -55,12 +56,13 @@ def Listen():
             print("Bekleme modundayım ve sizi dinliyorum...")
             r.adjust_for_ambient_noise(source)
             try:
-                voice = r.listen(source, timeout=7, phrase_time_limit=4)
+                voice = r.listen(source, timeout=40, phrase_time_limit=4)
                 voice_data = r.recognize_google(voice, language="tr-tr")
                 print(voice_data)
                 if wakeSentence in voice_data:
                     inConversation = True
-                    Talk("Efendim")
+                    Talk('Efendim')
+                    time.sleep(0.2)
             except sr.WaitTimeoutError:
                 pass
             except sr.UnknownValueError:
@@ -81,12 +83,23 @@ def Listen():
             print("Konuşabilirsiniz...")
             r.adjust_for_ambient_noise(source)
             try:
-                voice = r.listen(source, timeout=10, phrase_time_limit=10)
+                voice = r.listen(source, timeout=10, phrase_time_limit=5)
                 voice_data = r.recognize_google(voice, language="tr-tr")
-                print("Siz: " + str(voice_data))
+                #print("Şunu söylediniz: " + str(voice_data))
                 return voice_data
             except:
                 inConversation = False
 
 
+def BasicListen():
+    with sr.Microphone() as source:
+        print("Konuşabilirsiniz...")
+        r.adjust_for_ambient_noise(source)
+        try:
+            voice = r.listen(source, timeout=10, phrase_time_limit=5)
+            voice_data = r.recognize_google(voice, language="tr-tr")
+            print("Şunu söylediniz: " + str(voice_data))
+            return voice_data
+        except Exception as e:
+            print(e)
 # ------------------------- Program -------------------------
